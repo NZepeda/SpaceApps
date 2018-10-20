f212c1e9ccbb922c7bc7bc6cc8e24a4d5384069e
Nestor Zepeda <NestorDZepeda@gmail.com>
2018-10-20 16:13:16 -0400

    Adds axios to query server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Map from "./components/Map";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    this.getAllData();
+  }
+  getCircleOptions(
+    lat = 40.73061,
+    lng = -73.935242,
+    radius = Math.sqrt(1000) * 100
+  ) {
+    return {
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 0.35,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      center: { lat, lng },
+      radius: radius
+    };
+  }
+  getAllData() {
+    axios
+      .get("https://teasteroidm-api.herokuapp.com/api", {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(results => {
+        console.log(results);
+      });
+  }
   render() {
     return (
       <div className="App">
-        <Map map="hello" />
+        <Map cicleOptions={this.getCircleOptions()} />
       </div>
     );
   }
