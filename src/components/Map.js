adf6d8052712358e3d4ef43ff9c358ef62c6c29e
Nestor Zepeda <NestorDZepeda@gmail.com>
2018-10-20 16:34:35 -0400

    Adds mock crater

--- src/components/Map.js
+++ src/components/Map.js
@@ -13,6 +13,17 @@ const Map = withScriptjs(
       defaultCenter={{ lat: 40.73061, lng: -73.935242 }}
     >
       <Circle options={props.circleOptions} />
+      <Circle
+        options={{
+          strokeColor: "#FFFF00",
+          strokeOpacity: 0.8,
+          strokeWeight: 0.35,
+          fillColor: "#FFFF00",
+          fillOpacity: 0.35,
+          center: { lat: 40.73061, lng: -73.935242 },
+          radius: props.circleOptions.radius / 5
+        }}
+      />
     </GoogleMap>
   ))
 );
