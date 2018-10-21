c174dae2cabbdab0cf6e1f06dbd9115ed12a4d37
erinh816 <erinh816@gmail.com>
2018-10-21 11:00:30 -0400

    style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,115 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import axios from "axios";
+import Map from "./components/Map";
+import "./App.css";
 class App extends Component {
+  state = {
+    radius: "",
+    energy: "",
+    mass: "",
+    v_imp: "",
+    blastRadius: "",
+    fireball: "",
+    richterScale: ""
+  };
+  updateState(name, energy, mass, v_imp, blastRadius, fireball, richterScale) {
+    this.setState({
+      name,
+      energy,
+      mass,
+      v_imp,
+      blastRadius,
+      fireball,
+      richterScale
+    });
+  }
+  // updateStateRadius(radius, name) {
+  //   this.setState({ radius, name });
+  // }
+  // updateStateEnergy(energy, name) {
+  //   this.setState({ energy, name });
+  // }
+  // updateStateMass(mass, name) {
+  //   this.setState({ mass, name });
+  // }
+  componentDidMount() {
+    axios
+      .get("https://teasteroidm-api.herokuapp.com/api")
+      .then(json =>
+        json.data.map(result =>
+          this.updateState(
+            result.name,
+            result.energy,
+            result.mass,
+            result.v_imp,
+            result.blastRadius,
+            result.fireball,
+            result.richterScale
+          )
+        )
+      );
+    // .then(newData => console.log(newData));
+    //==========
+    this.getAllData();
+  }
+  getCircleOptions(
+    lat = 40.73061,
+    lng = -73.935242,
+    radius = Math.sqrt(1000) * 100
+  ) {
+    return {
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 0.35,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      center: { lat, lng },
+      radius: radius
+    };
+  }
+  getAllData() {
+    axios
+      .get("https://teasteroidm-api.herokuapp.com/api", {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(results => {
+        console.log(results);
+      });
+  }
   render() {
+    const blastRadius = this.state.blastRadius;
+    const energy = this.state.energy;
+    const mass = this.state.mass;
+    const vel = this.state.v_imp;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header />
+        {/* <Map circleOptions={this.getCircleOptions()} /> */}
+        <div id="knowledge">
+          <ul id="list">
+            <li>Energy Amount: {energy}</li>
+            <li>Mass: {mass}</li>
+            <li>Velocity: {vel}</li>
+            <li>Blast Radius: {blastRadius}</li>
+            <a href="https://www.nasa.gov/feature/jpl/nasa-and-fema-conduct-asteroid-impact-emergency-planning-exercise">
+              Emergency Preparedness Plan
+            </a>
+          </ul>
+        </div>
+        <div id="about" />
+        <p>
+          The Tracking Extraterrestrial Astroids and Meteorites (T.E.A.M) was
+          created for the purpose of identifying potential threats posed by
+          Astroids and Meteorites to Earth atmosphere. The primary objective of
+          the TEAM App's is to provide up-to-date critical information to in
+          order to educate communities around the world to be prepared to
+          mitigate the potential impact of Astroids related disasters.
+        </p>
       </div>
     );
   }
 }
-
 export default App;
